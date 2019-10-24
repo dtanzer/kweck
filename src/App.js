6c9e2b1c9ef718bd344fa8e9ed913df16e714468
David Tanzer <business@davidtanzer.net>
2019-10-24 20:31:25 +0200

    App has a config panel now.
    This does not do much yet, but it proves that I installed enzyme
    correctly and that my test do what I expect them to do (I got one
    test from red to green - The test that checks whether the app
    renders a config panel).

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Config } from './Config';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export function App() {
+	return (
+		<Config />
+	);
 }
-
-export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { App } from './App';
+import { Config } from './Config';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import { shallow, mount, render } from 'enzyme';
+
+describe('<App/>', () => {
+	it('renders the config panel', () => {
+		const app = shallow(<App />);
+		expect(app.find(Config)).toHaveLength(1);
+	});
 });
