f68e5b11864116bcb6d0e95910cf14657413b4d5
David Tanzer <business@davidtanzer.net>
2019-10-25 06:10:14 +0200

    Circle segment in timer uses calculated to values.
    The timer now contains a circle segment that draws the segment up
    to a point calculated by a function. This function, right now,
    always returns [0,-1] (the center/top point of the circle), but we
    can stub it in our tests.
    
    The actual segment is a higher-order component created by a function.
    This function gets a "circleSegment" function as a parameter, and now
    I can pass the stub in our tests. The Slice does not decide whether
    to draw the long or short side of the segment yet.
    
    The <Timer/> should later decide to which percentage of the circle
    the segment should be drawn, based on the current time, and pass
    that as a prop to <Slice/> (at least that's my plan right now).

--- src/Timer.js
+++ src/Timer.js
@@ -4,6 +4,7 @@ export function Timer() {
 	return (
 		<svg viewBox="-1.1 -1.1 2.2 2.2" width="400" height="400">
 			<Background />
+			<Slice />
 			<Foreground />
 		</svg>
 	);
@@ -15,4 +16,18 @@ export function Background() {
 
 export function Foreground() {
 	return <circle cx="0" cy="0" r="1" stroke="black" fill="none" strokeWidth="0.02"/>;
+}
+
+export const Slice = createSlice();
+
+function segmentTo() {
+	return [0, -1];
+}
+
+export function createSlice(circleSegment = segmentTo) {
+	return () => {
+		const [arcX, arcY] = circleSegment();
+		const pathData = `M 0 -1 A 1 1 0 0 1 ${arcX} ${arcY} L 0 0`;
+		return <path d={pathData} />
+	};
 }
--- src/Timer.test.js
+++ src/Timer.test.js
@@ -1,15 +1,31 @@
 import React from 'react';
-import { Timer, Background, Foreground } from './Timer';
+import { Timer, Background, Foreground, Slice, createSlice } from './Timer';
 
 import { shallow, mount, render } from 'enzyme';
+import sinon from 'sinon';
 
-describe('<Timer/>', () => {
-	it('renders the timer background', () => {
-		const timer = shallow(<Timer/>);
-		expect(timer.find(Background)).toHaveLength(1);
+describe('Component <Timer/> rendering', () => {
+	describe('<Timer/>', () => {
+		it('renders the timer background', () => {
+			const timer = shallow(<Timer/>);
+			expect(timer.find(Background)).toHaveLength(1);
+		});
+		it('renders the timer foreground', () => {
+			const timer = shallow(<Timer/>);
+			expect(timer.find(Foreground)).toHaveLength(1);
+		});
+		it('renders a timer slice', () => {
+			const timer = shallow(<Timer/>);
+			expect(timer.find(Slice)).toHaveLength(1);
+		});
 	});
-	it('renders the timer foreground', () => {
-		const timer = shallow(<Timer/>);
-		expect(timer.find(Foreground)).toHaveLength(1);
+
+	describe('<Slice/>', () => {
+		const circleSegment = sinon.fake.returns([12.5, 18.3]);
+		const SliceComp = createSlice(circleSegment);
+		const slice = shallow(<SliceComp />);
+
+		expect(slice.find('path')).toHaveLength(1);
+		expect(slice.find('path').at(0).prop('d')).toEqual('M 0 -1 A 1 1 0 0 1 12.5 18.3 L 0 0');
 	});
 });
