fa72b269af0fc1d5ceda958dfb069b5b2a379a47
David Tanzer <business@davidtanzer.net>
2019-10-26 06:56:02 +0200

    Moved timer styles to Timer.css

--- src/Timer.js
+++ src/Timer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Timer.css';
 import { percentFrom, segmentTo } from './segment';
 
 export const Timer = createTimer();
@@ -6,7 +7,7 @@ export const Timer = createTimer();
 export function createTimer(timeToPercentage = percentFrom) {
 	return ({ startMins=10, startSecs=0 }) => {
 		return (
-			<svg viewBox="-1.1 -1.1 2.2 2.2" width="400" height="400">
+			<svg className="timer" viewBox="-1.1 -1.1 2.2 2.2" width="400" height="400">
 				<Background />
 				<Slice percentLeft={timeToPercentage(startMins, startSecs)}/>
 				<Foreground />
@@ -16,11 +17,11 @@ export function createTimer(timeToPercentage = percentFrom) {
 }
 
 export function Background() {
-	return <circle cx="0" cy="0" r="1" stroke="none" fill="white"/>;
+	return <circle className="background" cx="0" cy="0" r="1" />;
 }
 
 export function Foreground() {
-	return <circle cx="0" cy="0" r="1" stroke="black" fill="none" strokeWidth="0.02"/>;
+	return <circle className="foreground" cx="0" cy="0" r="1" />;
 }
 
 export const Slice = createSlice();
@@ -29,6 +30,6 @@ export function createSlice(circleSegment = segmentTo) {
 	return ({percentLeft}) => {
 		const [arcX, arcY] = circleSegment(percentLeft);
 		const pathData = `M ${arcX} ${arcY} A 1 1 0 0 1 0 -1 L 0 0`;
-		return <path d={pathData} />
+		return <path className="slice" d={pathData} />
 	};
 }
