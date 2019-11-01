05ca8f8be86c1b7efa6b6ca1d2bf92431f67a373
David Tanzer <business@davidtanzer.net>
2019-11-01 07:07:08 +0100

    Timer does now keep the time on its own.
    It does not use the values calculated in Config anymore, so I can
    now start to remove the time keeping in Config.js.
    Also, time keeping is done differently than in Config, because it
    must use the useEffect hook (this was wrong in Config).

--- src/Timer.js
+++ src/Timer.js
@@ -1,22 +1,57 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Timer.css';
 import { percentFrom, segmentTo } from './segment';
 import { Config } from './Config';
 import { Background, Foreground, Slice } from './TimerComponents';
 
 
-export const Timer = createTimer();
+export const Timer = createTimer(percentFrom, () => {});
 
-export function createTimer(timeToPercentage = percentFrom) {
+
+export function createTimer(timeToPercentage = percentFrom, startTimer = setInterval, calcRemaining=calculateRemainingTime, currentTime=Date.now) {
 	return ({ startMins=10, startSecs=0 }) => {
+		const [runningState, setRunningState] = useState('stopped');
+		const [startTime, setStartTime] = useState(0);
+		const [countDownTime, setCountDownTime] = useState(10*60*1000);
+		const [remainingTime, setRemainingTime] = useState(0);
+
+		useEffect(() => {
+			startTimer(() => {
+				const elapsedTime = currentTime() - startTime;
+				setRemainingTime(countDownTime - elapsedTime);
+			}, 20);
+		}, [startTime, countDownTime]);
+
+		const onStart = () => {
+			setStartTime(currentTime());
+			setRunningState('running');
+		};
+
+		const calcTime = () => {
+			if(runningState === 'running') {
+				return calcRemaining(remainingTime);
+			}
+			return calcRemaining(countDownTime);
+		}
+		const { mins, secs } = calcTime();
+
 		return [
-			<Config />,
+			<Config startTimer={onStart} status={runningState} />,
 
 			<svg className="timer" viewBox="-1.1 -1.1 2.2 2.2" width="400" height="400">
 				<Background />
-				<Slice percentLeft={timeToPercentage(startMins, startSecs)}/>
+				<Slice percentLeft={timeToPercentage(mins, secs)}/>
 				<Foreground />
 			</svg>
 		];
 	};
+}
+
+export function calculateRemainingTime(milliseconds) {
+	const mins = Math.floor(milliseconds / (60*1000));
+	const secs = (milliseconds - mins*60*1000) / 1000;
+	return {
+		mins,
+		secs
+	}
 }
--- test/Timer.test.js
+++ test/Timer.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Timer, createTimer } from '../src/Timer';
+import { Timer, createTimer, calculateRemainingTime } from '../src/Timer';
 import { Background, Foreground, Slice } from '../src/TimerComponents';
 import { Config } from '../src/Config';
 
@@ -25,7 +25,7 @@ describe('Component <Timer/> rendering', () => {
 			expect(timer.find(Slice)).to.have.length(1);
 		});
 		it('passes percentLeft to slice when timer is not running', () => {
-			const timeToPercentage = sinon.fake.returns(17.34);
+			const timeToPercentage = () => 17.34;
 
 			const TimerComp = createTimer(timeToPercentage);
 			const timer = shallow(<TimerComp/>);
@@ -34,9 +34,84 @@ describe('Component <Timer/> rendering', () => {
 		it('passes the current time left to timeToPercentage', () => {
 			const timeToPercentage = sinon.fake.returns(0);
 
-			const TimerComp = createTimer(timeToPercentage);
-			const timer = shallow(<TimerComp startMins={6} startSecs={12.5} />);
+			const TimerComp = createTimer(timeToPercentage, ()=>{}, ()=>{ return {mins: 6, secs: 12.5, };});
+			const timer = shallow(<TimerComp />);
 			expect(timeToPercentage.calledWith(6, 12.5)).to.equal(true);
 		});
+		it('passes the running state to config when timer is stopped', () => {
+			const timer = shallow(<Timer />);
+			expect(timer.find(Config).prop('status')).to.equal('stopped');
+		});
+		it('passes the running state to config when timer is running', () => {
+			const timer = mount(<Timer />);
+
+			timer.find(Config).prop('startTimer')();
+			timer.update();
+
+			expect(timer.find(Config).prop('status')).to.equal('running');
+		});
 	});
+
+	describe('time keeping', () => {
+		const timeToPercentage = () => 0;
+
+		it('registeres a callback for timer ticks when start is pressed', () => {
+			const startTimer = sinon.stub();
+			const TimerComp = createTimer(timeToPercentage, startTimer);
+			
+			mount(<TimerComp />);
+
+			sinon.assert.called(startTimer);
+			expect(typeof(startTimer.getCall(0).args[0])).to.equal('function');
+		});
+
+		it('uses count down time to calculate minutes and seconds when timer is stopped', () => {
+			const startTimer = sinon.stub();
+			const calcRemaining = sinon.stub();
+			calcRemaining.returns({mins: 0, secs: 0 });
+			const TimerComp = createTimer(timeToPercentage, startTimer, calcRemaining);
+			
+			mount(<TimerComp />);
+
+			sinon.assert.calledWith(calcRemaining, 10*60*1000);
+		});
+		it('uses remaining time to calculate minutes and seconds when timer is stopped', () => {
+			const startTimer = sinon.stub();
+			const calcRemaining = sinon.stub();
+			calcRemaining.returns({mins: 0, secs: 0 });
+			const currentTime = sinon.stub();
+			currentTime.onCall(0).returns(0);
+			currentTime.onCall(1).returns(200);
+			const TimerComp = createTimer(timeToPercentage, startTimer, calcRemaining, currentTime);
+			
+			const timer = mount(<TimerComp />);
+			timer.find(Config).prop('startTimer')();
+			timer.update();
+			startTimer.getCall(0).args[0]();
+			timer.update();
+
+			sinon.assert.calledWith(calcRemaining, 10*60*1000-200);
+		});
+	});
+
+	describe('calculateRemainingTime', () => {
+		it('calculates the seconds correctly when minutes is 0', () => {
+			const {mins, secs} = calculateRemainingTime(2100);
+			expect(secs).to.be.closeTo(2.1, 0.001);
+		});
+		it('calculates the minutes correctly when seconds is 0', () => {
+			const {mins, secs} = calculateRemainingTime(120000);
+			expect(mins).to.equal(2);
+		});
+		it('calculates the minutes correctly when seconds is not 0', () => {
+			const {mins, secs} = calculateRemainingTime(121000);
+			expect(mins).to.equal(2);
+		});
+		it('calculates minutes and seconds correctly', () => {
+			const {mins, secs} = calculateRemainingTime(121050);
+			expect(mins).to.equal(2);
+			expect(secs).to.be.closeTo(1.05, 0.001);
+		})
+	});
+
 });
